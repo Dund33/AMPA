54f80225df586551b0d622e597968cf8524225af
Dund33 <warior1112@gmail.com>
2020-11-06 20:06:56 +0100

    Added switchable data banks and clear option for the repo. Added a new class for implementing tests

---
+++ app/src/androidTest/java/com/example/bmimaster/RepoTest.kt
@@ -0,0 +1,15 @@
+package com.example.bmimaster
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class RepoTest{
+    @Test
+    fun useAppContext() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        Assert.assertEquals("com.example.bmimaster", appContext.packageName)
+    }
+}
--- app/src/main/java/com/example/bmimaster/BMIRepo.kt
+++ app/src/main/java/com/example/bmimaster/BMIRepo.kt
@@ -8,7 +8,7 @@ import kotlin.collections.ArrayList
 object BMIRepo{
     private lateinit var sharedPreferences : SharedPreferences
     private var recordLength: Int = 0
-
+    var repoFile = "saves"
 
     fun init(context: Context, fileName: String, size: Int){
         sharedPreferences = context.getSharedPreferences(fileName, Context.MODE_PRIVATE)
@@ -25,11 +25,17 @@ object BMIRepo{
 
         val array = list.toTypedArray()
         val newListJSON = Gson().toJson(array)
-        sharedPreferences.edit().putString("saves", newListJSON).apply()
+        sharedPreferences.edit().putString(repoFile, newListJSON).apply()
+    }
+
+    fun clear(){
+        sharedPreferences.edit()
+            .putString(repoFile, "[]")
+            .apply()
     }
 
     private fun getBMIs(): ArrayList<BMIRecord> {
-        val listJSON = sharedPreferences.getString("saves", "[]")
+        val listJSON = sharedPreferences.getString(repoFile, "[]")
         val list =  Gson().fromJson(listJSON, Array<BMIRecord>::class.java)
         val arrayList = ArrayList<BMIRecord>()
         arrayList.addAll(list)
