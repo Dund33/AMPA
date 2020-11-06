69391bbdb320e62c86b6127651a6284c546394c0
Dund33 <warior1112@gmail.com>
2020-11-06 23:37:20 +0100

    Wrote unit tests for the BMI class

--- app/src/test/java/com/example/bmimaster/BMITests.kt
+++ app/src/test/java/com/example/bmimaster/BMITests.kt
@@ -1,26 +1,49 @@
 package com.example.bmimaster
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.data.forAll
+import io.kotest.matchers.doubles.plusOrMinus
 import io.kotest.matchers.floats.plusOrMinus
 import io.kotest.matchers.shouldBe
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.*
+import io.kotest.property.checkAll
 import org.junit.jupiter.api.Test
 
 class BMITests : FunSpec({
 
 
-    test("metric") {
+    test("metric single value") {
         val height = 180f
         val weight = 80f
-        val (bmi, color) = BMI.getBMI(height, weight, MeasureSystem.Metric)
+        val (bmi, _) = BMI.getBMI(height, weight, MeasureSystem.Metric)
         bmi shouldBe 24.7f.plusOrMinus(0.1f)
     }
 
-    test("imperial") {
+    test("imperial single value") {
         val height = 72f
         val weight = 170f
-        val (bmi, color) = BMI.getBMI(height, weight, MeasureSystem.Imperial)
+        val (bmi, _) = BMI.getBMI(height, weight, MeasureSystem.Imperial)
 
         bmi shouldBe 23.1f.plusOrMinus(0.1f)
+    }
+
+    //For some reason numericFloats won't work :<
+    test("metric many values"){
+        checkAll(Arb.numericDoubles(100.0, 200.0), Arb.numericDoubles(1.0, 200.0)){ height, weight ->
+            BMI.getBMI(height.toFloat(), weight.toFloat(), MeasureSystem.Metric).first.toDouble() shouldBe (weight/(height/100.0)/(height/100.0)).plusOrMinus(
+                0.5
+            )
+        }
+    }
+
+    //Same as above
+    test("imperial many values"){
+        checkAll(Arb.numericDoubles(50.0, 100.0), Arb.numericDoubles(100.0, 200.0)){ height, weight ->
+            BMI.getBMI(height.toFloat(), weight.toFloat(), MeasureSystem.Imperial).first.toDouble() shouldBe (weight/(height)/(height)*703).plusOrMinus(
+                0.5
+            )
+        }
     }
 })
 
